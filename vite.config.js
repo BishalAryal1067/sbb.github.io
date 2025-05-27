56d82474f8f9294bd6fd9dd4d91b4dc54bac03b6
BishalAryal1067 <bishalaryal1067@gmail.com>
2025-05-27 20:21:26 +0545

    fix: fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: "/sbb.github.io/"
+    base: "https://sbb-github-4q7zvk3v1-bishalaryal1067s-projects.vercel.app/"
 })
