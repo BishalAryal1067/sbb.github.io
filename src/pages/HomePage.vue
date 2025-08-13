1f88bb4aecc5639f4fb79426403a1ece7e59bebd
BishalAryal1067 <bishalaryal1067@gmail.com>
2025-08-13 21:14:59 +0545

    feat: added transition in the cards

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -39,36 +39,54 @@ import services from "../utils/../utils/services.js";
                 <p class="w-full text-xl sm:text-4xl text-center font-semibold text-brand-yellow">Why us / What do we
                     offer?</p>
                 <div class="sm:flex-row flex flex-col justify-center items-center gap-4 mt-5 sm:mt-12">
-                    <div class="flex flex-col gap-1 sm:gap-4 w-80 sm:h-90 overflow-hidden items-center text-center 
-                        border rounded-sm border-brand-yellow p-4">
+                    <div class="flex flex-col gap-4 
+                        sm:gap-4 w-80 sm:h-90 
+                        overflow-hidden items-center text-center 
+                        border rounded-sm border-brand-yellow p-4
+                        cursor-pointer
+                        transition-all hover:bg-brand-yellow hover:scale-92 group">
                         <img src="../assets/idea.svg" class="object-contain w-40 h-20">
-                        <p class="text-lg sm:text-2xl font-semibold text-brand-yellow">Cross-Sector Expertise</p>
+                        <p class="text-lg sm:text-2xl font-semibold text-brand-yellow group-hover:text-white">
+                            Cross-Sector Expertise</p>
                         <p class="font-normal sm:text-lg">We’ve worked with manufacturing units, service providers,
                             NGOs,
                             trading firms, and institutional bodies.</p>
                     </div>
 
-                    <div
-                        class="flex flex-col w-80 sm:h-90 overflow-hidden items-center text-center gap-4 border rounded-sm border-brand-yellow p-4">
+                    <div class="flex flex-col w-80 sm:h-90 
+                        overflow-hidden items-center text-center gap-4 
+                        border rounded-sm border-brand-yellow p-4
+                        cursor-pointer 
+                        transition-all hover:bg-brand-yellow hover:scale-92 group">
                         <img src="../assets/support.svg" class="object-contain w-40 h-20">
-                        <p class="text-lg sm:text-2xl font-semibold text-brand-yellow">Complete Business Solutions</p>
+                        <p class="text-lg sm:text-2xl font-semibold text-brand-yellow group-hover:text-white">
+                            Complete
+                            Business Solutions</p>
                         <p class="font-normal sm:text-lg">From setup to strategy, compliance to performance—we offer
                             holistic, end-to-end advisory services.</p>
                     </div>
 
-                    <div
-                        class="flex flex-col w-80 sm:h-90 overflow-hidden items-center text-center gap-4 border rounded-sm border-brand-yellow p-4">
+                    <div class="flex flex-col w-80 sm:h-90 
+                        overflow-hidden items-center text-center gap-4 
+                        border rounded-sm border-brand-yellow p-4
+                        cursor-pointer 
+                        transition-all hover:bg-brand-yellow hover:scale-92 group">
                         <img src="../assets/reliable.svg" class="object-contain w-40 h-20">
-                        <p class="text-lg sm:text-2xl font-semibold text-brand-yellow">Professionally Backed Team</p>
+                        <p class="text-lg sm:text-2xl font-semibold text-brand-yellow group-hover:text-white">
+                            Professionally Backed Team</p>
                         <p class="font-normal sm:text-lg">Supported by Chartered Accountants, Legal Experts, and
                             Industry
                             Advisors, ensuring depth and credibility in every solution.</p>
                     </div>
 
-                    <div
-                        class="flex flex-col w-80 sm:h-90 overflow-hidden items-center text-center gap-4 border rounded-sm border-brand-yellow p-4">
+                    <div class="flex flex-col w-80 sm:h-90 overflow-hidden 
+                        items-center text-center gap-4 border 
+                        rounded-sm border-brand-yellow p-4
+                        transition-all hover:bg-brand-yellow hover:scale-92 group">
                         <img src="../assets/reliable.svg" class="object-contain w-40 h-20">
-                        <p class="text-lg sm:text-2xl font-semibold text-brand-yellow">Outcome-Oriented Approach</p>
+                        <p class="text-lg sm:text-2xl font-semibold text-brand-yellow group-hover:text-white">
+                            Outcome-Oriented
+                            Approach</p>
                         <p class="font-normal sm:text-lg">
                             We prioritize measurable impact, sustainable growth, and long-term value creation for our
                             clients.
